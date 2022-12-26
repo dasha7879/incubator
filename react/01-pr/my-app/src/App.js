623b732e45ff753cb60df1ece31a2fbc90370716
dasha7879 <belka7879@gmail.com>
2022-12-26 16:12:49 +0300

    refactor: edit html

--- react/01-pr/my-app/src/App.js
+++ react/01-pr/my-app/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
+    <div>
+      <div>one div</div>
+      <div className="App">
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
     </div>
   );
 }
